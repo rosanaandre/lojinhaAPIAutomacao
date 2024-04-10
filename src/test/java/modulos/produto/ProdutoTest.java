189f123235d939596fd55c899bd63df2bac47ab9
Rosana Andre <kawaiicri@gmail.com>
2024-04-10 14:20:07 -0500

    Alterei a basePath para lojinha-bugada

--- src/test/java/modulos/produto/ProdutoTest.java
+++ src/test/java/modulos/produto/ProdutoTest.java
@@ -26,7 +26,7 @@ public class ProdutoTest {
         // Configurando os dados da API Rest da Lojinha
         baseURI = "http://165.227.93.41";
         // port = 8080;
-        basePath = "/lojinha";
+        basePath = "/lojinha-bugada";
 
 
         // Obter o token do usuario admin
